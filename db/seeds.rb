43b940b117bd761b7ce3a24a95db4cd783805821
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-01-28 13:21:06 +0000

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+import 'json'
+import 'open-uri'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "Cleanint DB!"
+Movie.destroy_all
+
+puts "Creating movies"
+movies["results"].each do | movie |
+  Movie.create(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+  end
+
+  puts "Done!"
