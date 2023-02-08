aa7894385571f1debf05b6be35c1aa3e1418fd32
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-02-08 10:10:04 +0000

    add route path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'lists#index'
+
   resources :lists, only: [:index, :new, :create, :show, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
